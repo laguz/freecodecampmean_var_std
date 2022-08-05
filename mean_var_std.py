9e3980c74ebd780832bf4f830bc82ff196a1a5d5
Luis Alberto Guzman Zorrilla <laguz3@gmail.com>
2022-08-04 20:41:20 -0400

    I have the first Python code

---
+++ mean_var_std.py
@@ -0,0 +1,11 @@
+from statistics import mean
+
+data1 = (11, 3, 4, 5, 7, 9, 2)
+
+print("Mean of data set 1 is % s" % (mean(data1)))
+
+def calculate():
+  print("This is Luis")
+  print("This is Lourdes")
+
+calculate()
