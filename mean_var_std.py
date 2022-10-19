b78a568a3a10a9b839ffca5a5401240e58bb587d
Luis Alberto Guzman Zorrilla <laguz3@gmail.com>
2022-10-18 23:36:52 -0400

    I add it the list.

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,9 @@
 from statistics import mean
 
 data1 = (11, 3, 4, 5, 7, 9, 2)
+my_list = [0,1,2,3,4,5,6,7,8]
 
-print("Mean of data set 1 is % s" % (mean(data1)))
+print("Mean of data set 1 is % s" % (mean(my_list)))
 
 def calculate():
   print("This is Luis")
